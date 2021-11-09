2638dc39be0b90d6367ecc11f6779f908973b1a6
zain391 <zainqamar391@gmail.com>
2021-11-10 03:06:36 +0500

    i have done the first task
    i have done the first task

--- style.js
+++ style.js
@@ -1,10 +1,13 @@
-function add(){
-    var head=document.getElementById("head");
-    
+let counter=0;
+let head=document.getElementById("head");
+let plus=document.getElementById("plus");
+let minus=document.getElementById("minus");
 
-}
-function minus(){
-    var head=document.getElementById("head");
-    
-
-} 
+plus.addEventListener("click",function(){
+    counter=counter + 1;
+    head.innerText=counter;
+});
+minus.addEventListener("click",function(){
+    counter=counter - 1;
+    head.innerText=counter;
+});
