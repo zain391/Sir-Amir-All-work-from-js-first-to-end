946c33a3dda9265fece942bf3b82371825d7e83f
zain391 <zainqamar391@gmail.com>
2021-11-11 07:31:10 +0500

    function for showing multiple output
    function for showing multiple output

---
+++ nov-11-2021/js-2nd-class/style.js
@@ -0,0 +1,74 @@
+ var display=document.getElementById("display");
+ display.addEventListener("click",function(){
+    console.log("i had to repeat this code so i am using the function.");
+    console.log("i had to repeat this code so i am using the function.");
+    console.log("i had to repeat this code so i am using the function.");
+    console.log("i had to repeat this code so i am using the function.");
+    console.clear();
+ });
+//  i am going to create an object
+ 
+
+
+
+// const plusButton = document.getElementById("plusButton");
+// const result = document.getElementById("result");
+// const inputOne = document.getElementById("numberOne");
+// const inputTwo = document.getElementById("numberTwo");
+// plusButton.onclick = function () {
+//   const valueOne = parseInt(inputOne.value);
+//   const valueTwo = parseInt(inputTwo.value);
+//   result.innerText = valueOne + valueTwo;
+// };
+// // const myPc = {
+// //   ram: 8,
+// // };
+// // // console.log(myPc);
+// // myPc.ram = 16;
+// // myPc.graphicCard = 6;
+// // // console.log(myPc);
+// // const myStudents = [
+// //   {
+// //     rollNo: 6642,
+// //     name: "Safyan",
+// //   },
+// //   {
+// //     rollNo: 6861,
+// //     name: "Ohad",
+// //   },
+// //   {
+// //     rollNo: 7078,
+// //     name: "Anas",
+// //   },
+// //   {
+// //     rollNo: 5158,
+// //     name: "Hamza",
+// //   },
+// // ];
+// // // console.log(myStudents[1].name);
+// // const mySelf = {
+// //   // property: value
+// //   // key: value
+// //   // key, value pair
+// //   name: "Amir Ali",
+// //   age: 24,
+// //   country: "Pakistan",
+// //   eating() {
+// //     console.log("I am eating rice");
+// //   },
+// //   // eating: function () {
+// //   //   console.log("I am eating rice");
+// //   // },
+// //   students: myStudents,
+// // };
+// // // Create a function that add 2 numbers
+// // function add(firstNumber, secondNumber) {
+// //   const result = firstNumber + secondNumber;
+// //   return { firstNumber: firstNumber, secondNumber, result };
+// // }
+// // const myResult = add(5, 3);
+// // console.log(myResult);
+// // console.log(add(5, 3)); // 12
+// // console.log(add(2, 2)); // 12
+// // console.log(add(6, 5)); // 12
+// // console.log(add(4, 3)); // 12
